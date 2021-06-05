d46cd62ba6dbcd5be067ef1771521c6a4c49ebbf
gotaek <64013443+gotaek@users.noreply.github.com>
2021-06-05 16:28:36 +0900

    create actor, director, writer table code

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-
 import pymysql
-import numpy as np
-import pandas as pd
 import csv
-import pymysql
-
-conn = pymysql.connect(host='localhost', user='gotaek',
-                       password='gotaek', db='movie')
-curs = conn.cursor(pymysql.cursors.DictCursor)
-
 
 f = open('title.akas.tsv', 'r', encoding='utf=8')
 rd = csv.reader(f, delimiter='\t')
 for line in rd:
-    sql = "insert into title(title)values(%s)"
-    curs.execute(sql, (line[2]))
-    conn.commit()
-    # print(line)
     print(line)
 f.close()
-curs.close()
-conn.close()
